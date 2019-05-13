cfa0fada6cfb9b0c9266ab871967be82bda963dd
meryem <meryem@energiedin.com>
2019-05-13 23:07:08 +0100

    nettoyage de dossier

--- public/script.js
+++
@@ -1,9 +0,0 @@
-$( "#connexion" ).click(function() {
- 
-  window.location.href = "http:../template/Backend/login.html";
-});
-
-$( "#accueil" ).click(function() {
- 
-  window.location.href = "http:../public/index.php";
-});
